d6b332d48fcfc9e3dd30ced563a9b9bb8f2199d9
DongFlexxia <dong@flexxia.ca>
2016-04-24 12:38:38 -0400

    add phpdebug

--- sites/all/modules/custom/jobboard/js/jobboard_jobs_map.js
+++ sites/all/modules/custom/jobboard/js/jobboard_jobs_map.js
@@ -11,10 +11,10 @@ jQuery(document).ready(function(){
     // create the tile layer with correct attribution
     var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+    var osm = new L.TileLayer(osmUrl, {minZoom: 6, maxZoom: 12, attribution: osmAttrib});
 
     // start the map in South-East England
-    map.setView(new L.LatLng(51.3, 0.7),9);
+    map.setView(new L.LatLng(42.17, -82.82), 10);
     map.addLayer(osm);
   }
 initmap();
---
+++ sites/all/modules/custom/phpdebug/import_export_term.php
@@ -0,0 +1,61 @@
+<?php
+
+$tree = taxonomy_get_tree(6);
+$output = '';
+
+foreach($tree as $key => $val){
+  $TermCityInfo = new TermCityInfo($val->tid);
+
+  $row = '';
+  if ($TermCityInfo) {
+    if ($TermCityInfo->tid > 2000) {
+      $row = array(
+        '"' . $TermCityInfo->termName() . '"',    // some string output
+        $TermCityInfo->provinceName(),
+        $TermCityInfo->latitude(),
+        $TermCityInfo->longitude(),
+      );
+
+      print_r('array(' . implode(',', $row) . '),');
+      // $output[] = implode(',', $row);
+      // $output[] = $row;
+    }
+  }
+}
+
+/** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+$citys = cityArray();
+foreach($citys as $key => $value){
+  $data = array(
+    'name' => $value[0],
+    'vid' => 18,
+  );
+  $term = entity_create('taxonomy_term', $data);
+
+  $wrapper = entity_metadata_wrapper('taxonomy_term', $term);
+  $wrapper->save();
+
+  // Note that the entity id (e.g., nid) must be passed as an integer not a string
+  $wrapper->field_city_province->set(intval($value[1]));
+  // set some customer field value
+  $wrapper->field_city_latlon_lat->set($value[2]);
+  $wrapper->field_city_latlon_lon->set($value[3]);
+  // save again
+  $wrapper->save();
+}
+
+/**
+ * import citys data
+ */
+function cityArray() {
+  $citys = array(
+    array("Addington",22,46.195426000000,-73.620458000000),
+    array("Yellowknife",24,62.453971700000,-114.371788600000),
+    array("York",27,43.695678700000,-79.450354400000),
+    array("Yorkton",30,51.213889000000,-102.462778000000),
+    array("Zorra",27,43.163405400000,-80.942891100000),
+  );
+
+  return $citys;
+}
