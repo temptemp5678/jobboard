731004d89467c47f98c8556e2983675243cab6fb
DongFlexxia <dong@flexxia.ca>
2016-05-06 09:46:05 -0400

    windsor coordinate

--- sites/all/modules/custom/jobboard/js/jobboard_jobs_map.js
+++ sites/all/modules/custom/jobboard/js/jobboard_jobs_map.js
@@ -6,33 +6,10 @@ jQuery(document).ready(function(){
   var plotlist;
   var plotlayers=[];
 
-  // function initmap() {
-  //   // set up the map, initialize the map object
-  //   map = new L.Map('mapid');
-
-  //   // create the tile layer with correct attribution
-  //   var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-
-  //   var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-
-  //   // Used L.tileLayer() to create a base layer of map tiles
-  //   var osm = new L.TileLayer(osmUrl, {
-  //     minZoom: 8,
-  //     maxZoom: 13,
-  //     attribution: osmAttrib
-  //   });
-
-  //   // setView() method to center the initial map
-  //   map.setView(new L.LatLng(42.32, -82.95), 11);
-  //   map.addLayer(osm);
-  // }
-
-  // initmap();
-
   // initialize the map
-  // var map = L.map('mapid').setView([42.32, -82.95], 11);          // Windsor
+  var map = L.map('mapid').setView([42.26, -82.95], 12);          // Windsor
   // var map = L.map('mapid').setView([42.35, -71.08], 11);          // Boston
-  var map = L.map('mapid').setView([39.911640, 116.401938], 12);  // Beijing
+  // var map = L.map('mapid').setView([39.911640, 116.401938], 12);  // Beijing
   // var map = L.map('mapid').setView([-41.2858, 174.78682], 14);       // Wellington
 
   // load a tile layer
@@ -42,37 +19,6 @@ jQuery(document).ready(function(){
      maxZoom: 17,
      minZoom: 6
    }).addTo(map);
-
-
-  // // load GeoJSON from an external file
-  // var geojsonFeature = [
-  //   {
-  //     "type": "Feature",
-  //     "properties": {
-  //         "name": "Coors Field",
-  //         "amenity": "Baseball Stadium",
-  //         "popupContent": "This is where the Rockies play!"
-  //     },
-  //     "geometry": {
-  //         "type": "Point",
-  //         "coordinates": [-82.95, 42.32]       // opposite
-  //     }
-  //   },
-  //   {
-  //     "type": "Feature",
-  //     "properties": {
-  //         "name": "Coors Field",
-  //         "amenity": "Baseball Stadium",
-  //         "popupContent": "This is where the Rockies play2!"
-  //     },
-  //     "geometry": {
-  //         "type": "Point",
-  //         "coordinates": [-82.98, 42.30]       // opposite
-  //     }
-  //   },
-
-  // ];
-  // L.geoJson(geojsonFeature).addTo(map);
 
   /** - - - - - - - Numbered Markers - - - - - - - - - - - - - - - - - - - - - - - -  */
   /**
@@ -117,22 +63,6 @@ jQuery(document).ready(function(){
   var windsorMarker1 = new L.Marker([42.33, -82.95], {
     icon: new L.NumberedDivIcon({number: '2'})
   }).addTo(map);
-
-  var beijingMarker1 = new L.Marker([39.8680001534,116.4951235621], {
-    icon: new L.NumberedDivIcon({number: '1'})
-  }).addTo(map);
-
-
-  /**
-   *
-   1. 北京欢乐谷
-   2. 北京海碗居老北京炸酱面(松榆里店)
-   3. 北京宏源南门涮肉 （天坛南门）
-   4. 和平门全聚德
-   5. 江边城外烤全鱼(交大店) （北京动物园北门外）
-   6. 姚记炒肝店（鼓楼包子）
-   7. 北京簋街
-   */
 
   // var jsonUrl = basePathUrl + "jsoninfo/jobboard/jobs/geomap.geojson";
   // jQuery.getJSON(jsonUrl, function(data){
