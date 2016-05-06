909d669ab647ab2ccceae4cb0e90ce305c1c5d01
DongFlexxia <dong@flexxia.ca>
2016-05-06 17:56:59 -0400

    add fxt theme

--- sites/all/themes/fxt/template.php
+++ sites/all/themes/fxt/template.php
@@ -5,8 +5,17 @@
  * template.php
  */
 
+/** - - - - - - base_path() - - - - - - - - - - - - - - - - - - - -  */
 /**
  * pass "base path" to JS
  */
 drupal_add_js('jQuery.extend(Drupal.settings, { "basePath": "' . base_path() . '" });', 'inline');
 drupal_add_js('jQuery.extend(Drupal.settings, { "currentPath": "' . current_path() . '" });', 'inline');
+
+/** - - - - - - current language url prefix - - - - - - - - - - - - - - - - - - - -  */
+global $language;
+if (isset($language->language)) {
+  if ($language->language == 'zh-hans' || $language->language == 'fr') {
+    drupal_add_js('jQuery.extend(Drupal.settings, { "currentLanguagePrefix": "' . $language->language . '" });', 'inline');
+  }
+}
